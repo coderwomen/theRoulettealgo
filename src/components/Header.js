3008f3f53f5fa523ba95a7e47da99373c0abca89
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-10-15 22:37:34 -0500

    💥💥💥 File components and Header component logic created

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserSimpleInfo from './UserSimpleInfo';
+
+const Header = () => {
+    //Logo
+    return(
+        <>
+            <UserSimpleInfo />
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/UserSimpleInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserName = styled.div`
+   
+`;
+
+const UserSimpleInfo = () => {
+    //Imagen de usuario
+    //icono de flecha
+    return(
+        <>
+        <UserName>Ana</UserName>
+        </>
+    );
+}
+
+export default UserSimpleInfo;
